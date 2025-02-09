c1ffdd0e1cc07bc7d8003d205a3929d8940c5acf
cbdyzj <cbdyzj@gmail.com>
2025-02-09 22:27:15 +0800

    Init

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import { createApp, h } from 'vue'
+import { createPinia } from 'pinia'
+import AppWithProvider from '@/components/AppWithProvider.vue'
+
+function startApp() {
+  const app = createApp({
+    render() {
+      return h(AppWithProvider)
+    },
+  })
+  app.use(createPinia())
+  const divRef = document.createElement('div')
+  document.body.appendChild(divRef)
+  app.mount(divRef)
+}
+
+startApp()
---
+++ src/userscript.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         more-ui
+// @namespace    https://alchem-x.github.io/more-ui/
+// @description  more ui
+// @author       Aerbeilu
+// @version      0.9
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
