f850d16d07207b8bd03fc7951af8ff25c0662270
cbdyzj <cbdyzj@gmail.com>
2025-02-09 22:27:15 +0800

    Init

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+import { resolve } from 'node:path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
+
+/**
+ * Define webpack config
+ *
+ * @returns {import('webpack').Configuration}
+ */
+export default function defineConfig(env) {
+  return {
+    mode: 'development',
+    entry: {
+      main: resolve(import.meta.dirname, 'src/main.js'),
+    },
+    module: {
+      rules: [
+        {
+          resourceQuery: /raw/,
+          type: 'asset/source',
+        },
+        {
+          resourceQuery: /^((?!raw).)*$/,
+          test: /\.jsx?$/i,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@emotion', '@vue/babel-plugin-jsx'],
+              },
+            },
+          ],
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: resolve(import.meta.dirname, 'src/index.html'),
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(import.meta.dirname, 'src'),
+      },
+    },
+    optimization: {
+      minimizer: [new TerserPlugin({ extractComments: false })],
+    },
+    output: {
+      filename: '[name].js',
+    },
+    performance: {
+      maxEntrypointSize: 1024 * 1024,
+      maxAssetSize: 1024 * 1024,
+    },
+  }
+}
