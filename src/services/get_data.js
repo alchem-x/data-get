bc45a6af692a6640f2a79940d63c6a716d8dc6a3
cbdyzj <cbdyzj@gmail.com>
2025-02-10 23:00:07 +0800

    AMend

--- src/services/get_data.js
+++ src/services/get_data.js
@@ -1,35 +1,71 @@
+import data from '../../data/res2.json'
+
+const dataQuery = `
+query reportDataQuery($input: GetReportDataInput) {
+  getReportData(input: $input) {
+    granularity
+    hadPrevious
+    hasNext
+    size
+    startDate
+    endDate
+    page
+    columns {
+      label
+      valueFormat
+      isGraphable
+      translationKey
+      isDefaultSortAscending
+      isDefaultGraphed
+      isDefaultSelected
+      isDefaultSortColumn
+      __typename
+    }
+    rows
+    __typename
+  }
+}
+`
 
 async function fetchData() {
-  const response = await fetch('https://sellercentral.amazon.com/business-reports/api',{
+  const response = await fetch('https://sellercentral.amazon.com/business-reports/api', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      "operationName": "reportDataQuery",
-      "variables": {
-        "input": {
-          "legacyReportId": "102:DetailSalesTrafficByChildItem",
-          "startDate": "2025-01-01",
-          "endDate": "2025-02-09",
-          "asins": []
-        }
+      operationName: 'reportDataQuery',
+      variables: {
+        input: {
+          legacyReportId: '102:DetailSalesTrafficByChildItem',
+          startDate: '2025-01-01',
+          endDate: '2025-02-09',
+          asins: [],
+        },
       },
-      "query": "query reportDataQuery($input: GetReportDataInput) {\n  getReportData(input: $input) {\n    granularity\n    hadPrevious\n    hasNext\n    size\n    startDate\n    endDate\n    page\n    columns {\n      label\n      valueFormat\n      isGraphable\n      translationKey\n      isDefaultSortAscending\n      isDefaultGraphed\n      isDefaultSelected\n      isDefaultSortColumn\n      __typename\n    }\n    rows\n    __typename\n  }\n}\n"
-    })
+      query: dataQuery.trim(),
+    }),
   })
   return await response.json()
+}
+
+function convertDataToCSV(data) {
+  const columns = data.data.getReportData.columns
+  const rows = data.data.getReportData.rows
+  const headerLine = columns.map((it) => it.label).join(',')
+  const content = rows.map((it) => it.join(',')).join('\n')
+  return headerLine + '\n' + content
 }
 
 export async function downloadData() {
   const { showSaveFilePicker } = window
   try {
-    const data = await fetchData()
+    // const data = await fetchData()
     const handle = await showSaveFilePicker({
-      suggestedName: 'data.txt',
+      suggestedName: 'data.csv',
     })
     const writable = await handle.createWritable()
-    await writable.write(new Blob([JSON.stringify(data)]))
+    await writable.write(new Blob([convertDataToCSV(data)]))
     await writable.close()
   } catch (err) {
     //
--- src/userscript.js
+++ src/userscript.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         data get script
 // @namespace    https://alchem-x.github.io/data-get/
-// @version      0.4
+// @version      0.5
 // @description  data get
 // @author       Aerbeilu
-// @require      https://alchem-x.github.io/data-get/main.js?0_4
+// @require      https://alchem-x.github.io/data-get/main.js?0_5
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
