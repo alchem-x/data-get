84fc87e9e4f9731367375d82cda1c90fccefdd82
cbdyzj <cbdyzj@gmail.com>
2025-02-10 22:38:53 +0800

    AMend

--- src/userscript.js
+++ src/userscript.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         data get script
 // @namespace    https://alchem-x.github.io/data-get/
-// @version      0.3
+// @version      0.4
 // @description  data get
 // @author       Aerbeilu
-// @require      https://alchem-x.github.io/data-get/main.js
+// @require      https://alchem-x.github.io/data-get/main.js?0_4
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
